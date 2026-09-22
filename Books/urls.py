dcf111c7d2896545b532690897bc5435a1b098c0
nur <n29320906@gmail.com>
2026-09-22 09:43:59 +0600

    add login url

--- Books/urls.py
+++ Books/urls.py
@@ -18,10 +18,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
+path(
+    "accounts/login/",
+    auth_views.LoginView.as_view(
+        template_name="registration/login.html"
+    ),
+    name="login",
+),
 ]
 
 
